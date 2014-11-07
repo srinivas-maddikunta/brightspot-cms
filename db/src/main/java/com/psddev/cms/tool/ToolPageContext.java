01793c06470d9172951dddaa02f52c0a694bc96d
Ryan-Anderson <ryan@perfectsensedigital.com>
2014-11-07 15:25:00 -0500

    adds evaporate to project and head

--- db/src/main/java/com/psddev/cms/tool/ToolPageContext.java
+++ db/src/main/java/com/psddev/cms/tool/ToolPageContext.java
@@ -1537,6 +1537,9 @@ public class ToolPageContext extends WebPageContext {
         writeStart("script", "type", "text/javascript", "src", cmsResource(jsPrefix + "d3.js"));
         writeEnd();
 
+        writeStart("script", "type", "text/javascript", "src", cmsResource(jsPrefix + "evaporate.js"));
+        writeEnd();
+
         writeStart("script", "type", "text/javascript");
             writeRaw("var require = ");
             writeRaw(ObjectUtils.toJson(ImmutableMap.of(
