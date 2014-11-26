1e533f3114c093caa0f937cfd157c69af40fe3ec
Ryan-Anderson <ryan@perfectsensedigital.com>
2014-11-26 11:37:11 -0500

    Simplifies S3 signing with StringUtils#hmacSha1()

--- db/src/main/java/com/psddev/cms/tool/S3SigningServlet.java
+++ db/src/main/java/com/psddev/cms/tool/S3SigningServlet.java
@@ -6,14 +6,11 @@ import com.psddev.dari.util.Settings;
 import com.psddev.dari.util.StringUtils;
 import org.apache.commons.codec.binary.Base64;
 
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.security.SignatureException;
 
 @RoutingFilter.Path(application = "cms", value = "s3auth")
 public class S3SigningServlet extends HttpServlet {
@@ -29,32 +26,10 @@ public class S3SigningServlet extends HttpServlet {
             throw new ServletException("dari/storage/psddevS3/secret not found in your context.xml");
         }
 
+        byte[] rawHmac = StringUtils.hmacSha1(secret, request.getParameter(TO_SIGN_PARAM));
+        String result = new String(Base64.encodeBase64(rawHmac));
+
         response.setContentType("text/html");
-        try {
-            response.getWriter().write(Signature.getSignature(request.getParameter(TO_SIGN_PARAM), secret));
-        } catch (SignatureException e) {
-            throw new ServletException("Unable to Sign request with your S3 secret");
-        }
-    }
-
-    private static class Signature {
-
-        private static final String HMAC_SHA1_ALGORITHM = "HmacSHA1";
-
-        public static String getSignature(String data, String key) throws SignatureException {
-            String result;
-            try {
-
-                SecretKeySpec signingKey = new SecretKeySpec(key.getBytes(), HMAC_SHA1_ALGORITHM);
-                Mac mac = Mac.getInstance(HMAC_SHA1_ALGORITHM);
-                mac.init(signingKey);
-                byte[] rawHmac = mac.doFinal(data.getBytes());
-                result = new String(Base64.encodeBase64(rawHmac));
-
-            } catch (Exception e) {
-                throw new SignatureException("Failed to generate HMAC : " + e.getMessage());
-            }
-            return result;
-        }
+        response.getWriter().write(result);
     }
 }
