423de54110fdf9b50743e55e967382f9a347c819
Ryan-Anderson <ryan@perfectsensedigital.com>
2015-10-08 13:55:31 -0400

    Updates ContentTypeValidator tests

--- db/src/test/java/com/psddev/cms/tool/file/ContentTypeValidatorTest.java
+++ db/src/test/java/com/psddev/cms/tool/file/ContentTypeValidatorTest.java
@@ -2,10 +2,16 @@ package com.psddev.cms.tool.file;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.io.IOUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +20,7 @@ import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
 import com.psddev.dari.util.AbstractStorageItem;
 import com.psddev.dari.util.Settings;
+import com.psddev.dari.util.SparseSet;
 import com.psddev.dari.util.StorageItem;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
@@ -40,8 +47,24 @@ public class ContentTypeValidatorTest {
 
     @Test(expected = IllegalStateException.class)
     public void invalidContentType() throws IOException {
+        Settings.setOverride("cms/tool/fileContentTypeGroups", "+image/png");
+        validator.validate(file, "image/jpeg");
+    }
+
+    @Test
+    public void validContentType() throws IOException {
         String fileContentType = "image/jpeg";
-        Settings.setOverride("cms/tool/fileContentTypeGroups", fileContentType);
+        Set<String> contentTypeGroups = new SparseSet();
+        contentTypeGroups.add(fileContentType);
+        contentTypeGroups.add("text/html");
+        Settings.setOverride("cms/tool/fileContentTypeGroups", contentTypeGroups.toString());
         validator.validate(file, fileContentType);
+    }
+
+    @Test(expected = IOException.class)
+    public void disguisedHtmlFile() throws IOException, URISyntaxException {
+        File htmlFile = new File(getClass().getClassLoader().getResource("html-image.jpg").toURI());
+        Settings.setOverride("cms/tool/fileContentTypeGroups", "+image/jpeg");
+        validator.validate(htmlFile, "image/jpeg");
     }
 }
