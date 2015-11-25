4c3a752dc2490e9dff92ba1a8a596537292e4fa9
Ryan-Anderson <ryan@perfectsensedigital.com>
2015-11-25 10:12:31 -0500

    Adds additional javadocs to FileContentType

--- db/src/main/java/com/psddev/cms/tool/FileContentType.java
+++ db/src/main/java/com/psddev/cms/tool/FileContentType.java
@@ -23,10 +23,28 @@ public interface FileContentType {
      */
     double getPriority(StorageItem storageItem);
 
+    /**
+     * Enables additional processing of a {@link StorageItem}
+     * during the POST request by {@link com.psddev.cms.tool.page.content.field.StorageItemField}.
+     *
+     * @param page Can't be {@code null}
+     * @param storageItem StorageItem value for the field
+     */
     default void process(ToolPageContext page, StorageItem storageItem) {
 
     }
 
+    /**
+     * Enables custom preview rendering for a {@link StorageItem} field in the CMS.
+     * Executed only by a GET request in {@link com.psddev.cms.tool.page.content.field.StorageItemField}.
+     *
+     * @param page Can't be {@code null}.
+     * @param state State of the object with the StorageItem field
+     * @param fieldValue StorageItem value for the field
+     * 
+     * @throws IOException
+     * @throws ServletException
+     */
     void writePreview(ToolPageContext page, State state, StorageItem fieldValue) throws IOException, ServletException;
 
     static FileContentType getFileContentType(StorageItem storageItem) {
