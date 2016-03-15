ed1b64be53bb77fb4d64526b45fac8bf0e3bf0b6
Hyoo Lim <hyoolim@gmail.com>
2016-03-14 21:52:02 -0400

    Changes drop down to always display above other things.

--- tool-ui/src/main/webapp/script/v3/jquery.dropdown.js
+++ tool-ui/src/main/webapp/script/v3/jquery.dropdown.js
@@ -152,7 +152,7 @@ define(['string'], function (S) {
       containerCss = {
         'display': 'none',
         'position': isFixedPosition ? 'fixed' : 'absolute',
-        'z-index': $original.zIndex()
+        'z-index': $original.zIndex() + 1000000
       };
 
       $markerContainer = $('<div/>', {
