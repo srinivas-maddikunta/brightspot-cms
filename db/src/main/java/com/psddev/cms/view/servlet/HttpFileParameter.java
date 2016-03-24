96992171ddb0a659ec283ad77607ff58bd097871
Axel Tarnvik <axel@perfectsensedigital.com>
2016-03-24 13:14:43 -0400

    BSP-1838 adds HttpFileParameter

---
+++ db/src/main/java/com/psddev/cms/view/servlet/HttpFileParameter.java
@@ -0,0 +1,46 @@
+package com.psddev.cms.view.servlet;
+
+import com.psddev.dari.util.ObjectUtils;
+import com.psddev.dari.util.StorageItem;
+import com.psddev.dari.util.StorageItemFilter;
+import com.psddev.dari.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Populates a StorageItem with the date from the query parameter from an HTTP request. The
+ * parameter fetched has the same name as the field it populates unless
+ * otherwise specified.
+ */
+@ServletViewRequestAnnotationProcessorClass(HttpFileParameterProcessor.class)
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface HttpFileParameter {
+    String value() default "";
+}
+
+class HttpFileParameterProcessor implements ServletViewRequestAnnotationProcessor<HttpFileParameter> {
+
+    @Override
+    public StorageItem getValue(HttpServletRequest request, String fieldName, HttpFileParameter annotation) {
+        String parameterName = annotation.value();
+        if (StringUtils.isBlank(parameterName)) {
+            parameterName = fieldName;
+        }
+        try {
+            if (ObjectUtils.isBlank(request.getParameter(parameterName))) {
+                return null;
+            }
+            return StorageItemFilter.getParameter(request, parameterName, null);
+        } catch (IOException e) {
+            return null;
+        }
+    }
+}
