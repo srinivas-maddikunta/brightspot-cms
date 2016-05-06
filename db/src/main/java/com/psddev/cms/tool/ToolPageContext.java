2e0e2031c08bab7367a41bf252d2336121cae3ec
Hyoo Lim <hyoolim@gmail.com>
2016-05-06 14:55:18 -0400

    Prevents abstract types from showing up in new type selector.

--- db/src/main/java/com/psddev/cms/tool/ToolPageContext.java
+++ db/src/main/java/com/psddev/cms/tool/ToolPageContext.java
@@ -2358,9 +2358,25 @@ public class ToolPageContext extends WebPageContext {
 
         writeTypeSelectReally(
                 true,
+                false,
                 types,
                 selectedTypes != null ? selectedTypes : Collections.<ObjectType>emptySet(),
                 null,
+                attributes);
+    }
+
+    public void writeCreateTypeSelect(
+            Iterable<ObjectType> types,
+            ObjectType selectedType,
+            String allLabel,
+            Object... attributes) throws IOException {
+
+        writeTypeSelectReally(
+                false,
+                true,
+                types,
+                selectedType != null ? Arrays.asList(selectedType) : Collections.<ObjectType>emptySet(),
+                allLabel,
                 attributes);
     }
 
@@ -2382,6 +2398,7 @@ public class ToolPageContext extends WebPageContext {
             Object... attributes) throws IOException {
 
         writeTypeSelectReally(
+                false,
                 false,
                 types,
                 selectedType != null ? Arrays.asList(selectedType) : Collections.<ObjectType>emptySet(),
@@ -2409,6 +2426,7 @@ public class ToolPageContext extends WebPageContext {
 
     private void writeTypeSelectReally(
             boolean multiple,
+            boolean create,
             Iterable<ObjectType> types,
             Collection<ObjectType> selectedTypes,
             String allLabel,
@@ -2420,10 +2438,12 @@ public class ToolPageContext extends WebPageContext {
 
         List<ObjectType> miscTypes = ObjectUtils.to(new TypeReference<List<ObjectType>>() { }, types);
 
-        for (ObjectType type : Database.Static.getDefault().getEnvironment().getTypes()) {
-            if (Boolean.FALSE.equals(type.as(ToolUi.class).getHidden()) && !type.isConcrete()) {
-                if (miscTypes.containsAll(type.findConcreteTypes())) {
-                    miscTypes.add(type);
+        if (!create) {
+            for (ObjectType type : Database.Static.getDefault().getEnvironment().getTypes()) {
+                if (Boolean.FALSE.equals(type.as(ToolUi.class).getHidden()) && !type.isConcrete()) {
+                    if (miscTypes.containsAll(type.findConcreteTypes())) {
+                        miscTypes.add(type);
+                    }
                 }
             }
         }
--- tool-ui/src/main/webapp/WEB-INF/search.jsp
+++ tool-ui/src/main/webapp/WEB-INF/search.jsp
@@ -630,7 +630,6 @@ writer.start("div", "class", "searchForm");
         writer.end();
 
         if (!ObjectUtils.isBlank(newJsp)
-                && (selectedType == null || !selectedType.isAbstract())
                 && (!singleType || !selectedType.as(ToolUi.class).isReadOnly())) {
             writer.start("div", "class", "searchCreate");
                 writer.start("h2").html("Create").end();
@@ -659,7 +658,7 @@ writer.start("div", "class", "searchForm");
                             }
                         }
 
-                        wp.writeTypeSelect(
+                        wp.writeCreateTypeSelect(
                                 creatableTypes.stream()
                                     .filter(t -> !t.as(ToolUi.class).isReadOnly())
                                     .filter(wp.createTypeDisplayPredicate(ImmutableSet.of("write", "read")))
