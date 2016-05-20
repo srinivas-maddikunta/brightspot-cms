af97a8f489ff7afd76958041a4a1f3561ae4d33f
Patrick Fitzgerald <pat@perfectsensedigital.com>
2016-05-20 16:47:50 -0400

    RTE prevent preview fetch unless style has flag
    To improve performance, avoid fetching preview data on all line styles, instead fetch preview data only for styles that support it.
    This commit adds a check to the updatePreview() function, so only styles that have a previewable:true flag are fetched. This is the FE portion of this code only, it is assumed that the style definitions in the global RICH_TEXT_ELEMENTS variable will be modified to set a previewable flag for the styles that support preview.

--- tool-ui/src/main/webapp/script/v3/input/richtext2.js
+++ tool-ui/src/main/webapp/script/v3/input/richtext2.js
@@ -4283,7 +4283,8 @@ define(['jquery', 'v3/input/richtextCodeMirror', 'v3/input/tableEditor', 'v3/plu
                 context: rtElement.context,
                 keymap: rtElement.keymap,
                 clear: rtElement.clear,
-                toggle: rtElement.toggle
+                toggle: rtElement.toggle,
+                previewable: Boolean(rtElement.previewable)
             };
         });
     }
@@ -4325,13 +4326,20 @@ define(['jquery', 'v3/input/richtextCodeMirror', 'v3/input/tableEditor', 'v3/plu
 
             rte = Object.create(Rte);
             rte.init(input, options);
-
+            
             function updatePreview() {
                 rte.rte.blockEachLineMark(function (name, mark) {
                     var className = mark.className;
+                    var styleObj = rte.rte.classes[className];
                     var attributesJson = JSON.stringify(mark.attributes);
                     var newPreviewKey = className + attributesJson;
 
+                    // Skip this mark if it doesn't match one of our styles,
+                    // or if the style does not have the previewable flag
+                    if (!styleObj || !styleObj.previewable) {
+                        return;
+                    }
+                    
                     if (mark.rtePreviewKey !== newPreviewKey) {
                         mark.rtePreviewKey = newPreviewKey;
 
