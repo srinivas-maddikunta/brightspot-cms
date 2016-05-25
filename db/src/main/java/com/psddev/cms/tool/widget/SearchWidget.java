7612927d6d02c44b27d4f4d13162899f63e76648
Hyoo Lim <hyoolim@gmail.com>
2016-05-25 14:32:43 -0400

    Adds search dashboard widget.

---
+++ db/src/main/java/com/psddev/cms/tool/widget/SearchWidget.java
@@ -0,0 +1,67 @@
+package com.psddev.cms.tool.widget;
+
+import com.psddev.cms.tool.CmsTool;
+import com.psddev.cms.tool.Dashboard;
+import com.psddev.cms.tool.DashboardWidget;
+import com.psddev.cms.tool.Search;
+import com.psddev.cms.tool.ToolPageContext;
+import com.psddev.dari.db.ObjectType;
+import com.psddev.dari.util.JspUtils;
+import com.psddev.dari.util.StringUtils;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class SearchWidget extends DashboardWidget {
+
+    @Required
+    private String heading;
+
+    private Set<ObjectType> types;
+
+    public String getHeading() {
+        return heading;
+    }
+
+    public void setHeading(String heading) {
+        this.heading = heading;
+    }
+
+    public Set<ObjectType> getTypes() {
+        if (types == null) {
+            types = new LinkedHashSet<>();
+        }
+        return types;
+    }
+
+    public void setTypes(Set<ObjectType> types) {
+        this.types = types;
+    }
+
+    @Override
+    public void writeHtml(ToolPageContext page, Dashboard dashboard) throws IOException, ServletException {
+        page.writeStart("div", "class", "widget SearchWidget");
+        {
+            page.writeStart("h1");
+            page.writeHtml(getHeading());
+            page.writeEnd();
+
+            String searchPath = page.toolPath(CmsTool.class, "/WEB-INF/search.jsp");
+
+            for (ObjectType type : getTypes()) {
+                searchPath = StringUtils.addQueryParameters(searchPath, Search.TYPES_PARAMETER, type.getId());
+            }
+
+            JspUtils.include(
+                    page.getRequest(),
+                    page.getResponse(),
+                    page,
+                    searchPath,
+                    "name", "fullScreen",
+                    "resultJsp", "/misc/searchResult.jsp");
+        }
+        page.writeEnd();
+    }
+}
