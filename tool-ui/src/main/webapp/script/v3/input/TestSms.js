ed120690174d3d09d8b9f87ecdd043785261f339
Rohit S Monga <rmonga@perfectsensedigital.com>
2016-08-29 17:06:56 -0400

    Updates TestSms.js to execute when page is fully loaded.

--- tool-ui/src/main/webapp/script/v3/input/TestSms.js
+++ tool-ui/src/main/webapp/script/v3/input/TestSms.js
@@ -1,38 +1,40 @@
 require(['jquery'], function($) {
+    $(window).load(function() {
 
-    const SUCCESS_RESPONSE = 'Test message sent!';
-    const FAILURE_RESPONSE = 'Unable to send test message!';
+        const SUCCESS_RESPONSE = 'Test message sent!';
+        const FAILURE_RESPONSE = 'Unable to send test message!';
 
-    var $smsButton = $('.smsButton');
-    var $smsResponseText = $('.smsResponseText');
-    var $phoneNumber = $smsButton.parent().find('.inputSmall').find('textarea');
+        var $smsButton = $('.smsButton');
+        var $smsResponseText = $('.smsResponseText');
+        var $phoneNumber = $smsButton.parent().find('.inputSmall').find('textarea');
 
-    $smsButton.on('click', function (e) {
-        e.preventDefault();
-        $.ajax({
-            url: CONTEXT_PATH + 'testSms',
-            type: 'POST',
-            data: {
-                phoneNumber: $phoneNumber.val()
-            },
-            beforeSend: function () {
-                $smsButton.hide();
-                $smsResponseText.addClass('smsPendingResponse');
-            },
-            error: function () {
-                $smsResponseText.removeClass('smsPendingResponse');
-                $smsResponseText.addClass('smsErrorResponse');
-                $smsResponseText.text(FAILURE_RESPONSE);
-            },
-            success: function (data) {
-                $smsResponseText.removeClass('smsPendingResponse');
-                if (data == SUCCESS_RESPONSE) {
-                    $smsResponseText.addClass('smsSuccessResponse');
-                } else if (data == FAILURE_RESPONSE) {
+        $smsButton.on('click', function (e) {
+            e.preventDefault();
+            $.ajax({
+                url: CONTEXT_PATH + 'testSms',
+                type: 'POST',
+                data: {
+                    phoneNumber: $phoneNumber.val()
+                },
+                beforeSend: function () {
+                    $smsButton.hide();
+                    $smsResponseText.addClass('smsPendingResponse');
+                },
+                error: function () {
+                    $smsResponseText.removeClass('smsPendingResponse');
                     $smsResponseText.addClass('smsErrorResponse');
+                    $smsResponseText.text(FAILURE_RESPONSE);
+                },
+                success: function (data) {
+                    $smsResponseText.removeClass('smsPendingResponse');
+                    if (data == SUCCESS_RESPONSE) {
+                        $smsResponseText.addClass('smsSuccessResponse');
+                    } else if (data == FAILURE_RESPONSE) {
+                        $smsResponseText.addClass('smsErrorResponse');
+                    }
+                    $smsResponseText.text(data);
                 }
-                $smsResponseText.text(data);
-            }
+            });
         });
     });
 });
