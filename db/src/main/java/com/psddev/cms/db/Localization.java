0a91195c8d38a566b9c78d3832730ed20d322f9c
Hyoo Lim <hyoolim@gmail.com>
2016-09-23 11:47:26 -0400

    Adds special, frequently used skeletons for Localization#date methods.

--- db/src/main/java/com/psddev/cms/db/Localization.java
+++ db/src/main/java/com/psddev/cms/db/Localization.java
@@ -7,10 +7,24 @@ import com.psddev.cms.tool.AuthenticationFilter;
 import com.psddev.dari.util.PageContextFilter;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
+import java.time.temporal.ChronoField;
 import java.util.Date;
 import java.util.Locale;
 
 public final class Localization {
+
+    public static final String NOT_AVAILABLE_KEY = "label.notAvailable";
+
+    public static final String DATE_AND_TIME_SKELETON = "_dateAndTime";
+
+    public static final String DATE_ONLY_SKELETON = "_dateOnly";
+
+    public static final String TIME_ONLY_SKELETON = "_timeOnly";
+
+    private static final String ICU_DATE_SKELETON = "EEE MMM dd";
+    private static final String ICU_DATE_WITH_YEAR_SKELETON = "EEE MMM dd yyyy";
+    private static final String ICU_TIME_SKELETON = "hh:mm aa";
 
     public static String text(Locale locale, Object context, String key) {
         LocalizationContext localizationContext = context instanceof LocalizationContext
@@ -59,27 +73,49 @@ public final class Localization {
 
     /**
      * Localizes the given {@code timeMillis} in the given {@code timeZoneId}
-     * using the given {@code locale} and {@code format}.
+     * using the given {@code locale} and {@code skeleton}.
      *
      * @param locale Nullable. Default is {@link Locale#getDefault()}.
      * @param timeZoneId Nullable. Default is {@link java.util.TimeZone#getDefault()}.
-     * @param format Nonnull.
+     * @param skeleton Nonnull.
      * @return Nonnull.
      */
-    public static String date(Locale locale, String timeZoneId, long timeMillis, String format) {
-        Preconditions.checkNotNull(format);
+    public static String date(Locale locale, String timeZoneId, long timeMillis, String skeleton) {
+        Preconditions.checkNotNull(skeleton);
 
         if (locale == null) {
             locale = Locale.getDefault();
+        }
+
+        if (timeMillis < 0) {
+            return text(locale, null, NOT_AVAILABLE_KEY);
         }
 
         if (timeZoneId == null) {
             timeZoneId = java.util.TimeZone.getDefault().getID();
         }
 
-        DateFormat dateFormat = DateFormat.getPatternInstance(format, locale);
-        dateFormat.setTimeZone(TimeZone.getTimeZone(timeZoneId));
-        return dateFormat.format(new Date(timeMillis));
+        Date time = new Date(timeMillis);
+
+        if (DATE_AND_TIME_SKELETON.equals(skeleton)) {
+            skeleton = dateSkeleton(timeMillis) + " " + ICU_TIME_SKELETON;
+
+        } else if (DATE_ONLY_SKELETON.equals(skeleton)) {
+            skeleton = dateSkeleton(timeMillis);
+
+        } else if (TIME_ONLY_SKELETON.equals(skeleton)) {
+            skeleton = ICU_TIME_SKELETON;
+        }
+
+        DateFormat format = DateFormat.getPatternInstance(skeleton, locale);
+        format.setTimeZone(TimeZone.getTimeZone(timeZoneId));
+        return format.format(new Date(timeMillis));
+    }
+
+    private static String dateSkeleton(long timeMillis) {
+        return Instant.now().get(ChronoField.YEAR) == Instant.ofEpochMilli(timeMillis).get(ChronoField.YEAR)
+                ? ICU_DATE_SKELETON
+                : ICU_DATE_WITH_YEAR_SKELETON;
     }
 
     public static String currentUserText(Object context, String key) {
