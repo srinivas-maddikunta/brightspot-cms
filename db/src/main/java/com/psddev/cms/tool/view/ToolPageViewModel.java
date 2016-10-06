e2259281579f1bfab70e5eb2d620ceb6071765b4
Ryan-Anderson <ryan@perfectsensedigital.com>
2016-10-06 10:50:09 -0400

    Updates ToolPageViewModel to use consumer pattern

--- db/src/main/java/com/psddev/cms/tool/view/ToolPageViewModel.java
+++ db/src/main/java/com/psddev/cms/tool/view/ToolPageViewModel.java
@@ -2,40 +2,42 @@ package com.psddev.cms.tool.view;
 
 import java.io.IOException;
 import java.io.StringWriter;
-import java.io.Writer;
 
 import com.psddev.cms.tool.ToolPageContext;
 import com.psddev.cms.view.RawView;
 import com.psddev.cms.view.ViewModel;
+import com.psddev.dari.util.PageContextFilter;
 
 /**
  * Abstract {@link ViewModel} to help render Views for {@link com.psddev.cms.tool.Tool} pages.
  */
 public abstract class ToolPageViewModel<M> extends ViewModel<M> {
 
-    @CurrentToolPageContext
-    protected ToolPageContext page;
-
     /**
-     * Helper method for getting HTML produced by @{link ToolPageContext}#write methods.
+     * Helper method to produce a {@link RawView} from
+     * {@link ToolPageContext}#write... methods.
      */
-    protected RawView getRawView(ToolPageWriter writer) {
-        Writer oldDelegate = page.getDelegate();
-        StringWriter newDelegate = new StringWriter();
+    protected RawView createRawView(RawViewWriter<ToolPageContext> writer) {
+        StringWriter delegate = new StringWriter();
+
+        ToolPageContext page = new ToolPageContext(
+                PageContextFilter.Static.getServletContext(),
+                PageContextFilter.Static.getRequest(),
+                PageContextFilter.Static.getResponse());
+        page.setDelegate(delegate);
+
         try {
-            page.setDelegate(newDelegate);
-            writer.write();
-        } catch (Exception e) {
+            writer.write(page);
+        } catch (IOException e) {
             // This should never happen.
             throw new IllegalStateException(e);
-        } finally {
-            page.setDelegate(oldDelegate);
         }
 
-        return RawView.of(newDelegate.toString());
+        return RawView.of(delegate.toString());
     }
 
-    protected interface ToolPageWriter {
-        void write() throws IOException;
+    @FunctionalInterface
+    protected interface RawViewWriter<T> {
+        void write(T t) throws IOException;
     }
 }
