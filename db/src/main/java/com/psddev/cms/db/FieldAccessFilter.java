a2ee736d793d19cb8ed00613f3bd5dfb72404a9d
Hyoo Lim <hyoolim@gmail.com>
2016-10-14 14:23:11 -0400

    Moves all FieldAccessFilter static methods.

--- db/src/main/java/com/psddev/cms/db/FieldAccessFilter.java
+++ db/src/main/java/com/psddev/cms/db/FieldAccessFilter.java
@@ -31,9 +31,64 @@ import com.psddev.dari.util.ThreadLocalStack;
 public class FieldAccessFilter extends AbstractFilter {
 
     private static final String ATTRIBUTE_PREFIX = FieldAccessFilter.class.getName() + ".";
+    private static final String DISPLAY_IDS_ATTRIBUTE = ATTRIBUTE_PREFIX + "displayIds";
     private static final String CURRENT_RESPONSE_ATTRIBUTE = ATTRIBUTE_PREFIX + "currentResponse";
 
     private static final ThreadLocalStack<LazyWriter> THREAD_DEFAULT_LAZY_WRITER = new ThreadLocalStack<>();
+
+    /**
+     * Returns IDs of of all objects whose field accesses should be
+     * displayed.
+     *
+     * @param request Nonnull.
+     * @return Nonnull.
+     */
+    public static Set<UUID> getDisplayIds(HttpServletRequest request) {
+        @SuppressWarnings("unchecked")
+        Set<UUID> displayIds = (Set<UUID>) request.getAttribute(DISPLAY_IDS_ATTRIBUTE);
+
+        if (displayIds == null) {
+            displayIds = new HashSet<>();
+
+            request.setAttribute(DISPLAY_IDS_ATTRIBUTE, displayIds);
+        }
+
+        return displayIds;
+    }
+
+    /**
+     * Creates the marker HTML that identifies access to a field with the
+     * given {@code name} in the given {@code state}.
+     *
+     * @param state Nonnull.
+     * @param name Nonnull.
+     * @return Nonnull.
+     */
+    public static String createMarkerHtml(State state, String name) {
+        Map<String, Object> markerData = new CompactMap<>();
+        ObjectType type = state.getType();
+
+        if (type != null) {
+            ObjectField field = type.getField(name);
+
+            if (field != null) {
+                String fieldType = field.getInternalType();
+
+                if (ObjectField.TEXT_TYPE.equals(fieldType)) {
+                    Object value = state.get(name);
+
+                    if (value != null) {
+                        markerData.put("text", value.toString());
+                    }
+                }
+            }
+        }
+
+        markerData.put("id", state.getId().toString());
+        markerData.put("name", name);
+
+        return "<!--BrightspotCmsFieldAccess " + ObjectUtils.toJson(markerData) + "-->";
+    }
 
     /**
      * Writes using the given {@code consumer}, inserting field access markers
@@ -170,65 +225,27 @@ public class FieldAccessFilter extends AbstractFilter {
     }
 
     /**
-     * {@link FieldAccessFilter} utility methods.
+     * @deprecated
+     * @see FieldAccessFilter
      */
     public static class Static {
 
-        private static final String ATTRIBUTE_PREFIX = Static.class.getName() + ".";
-        private static final String DISPLAY_IDS_ATTRIBUTE = ATTRIBUTE_PREFIX + "displayIds";
-
         /**
-         * Creates the marker HTML that identifies access to a field with the
-         * given {@code name} in the given {@code state}.
-         *
-         * @param state Can't be {@code null}.
-         * @param name Can't be {@code null}.
-         * @return Never blank.
+         * @deprecated
+         * @see FieldAccessFilter#createMarkerHtml(State, String)
          */
+        @Deprecated
         public static String createMarkerHtml(State state, String name) {
-            Map<String, Object> markerData = new CompactMap<>();
-            ObjectType type = state.getType();
-
-            if (type != null) {
-                ObjectField field = type.getField(name);
-
-                if (field != null) {
-                    String fieldType = field.getInternalType();
-
-                    if (ObjectField.TEXT_TYPE.equals(fieldType)) {
-                        Object value = state.get(name);
-
-                        if (value != null) {
-                            markerData.put("text", value.toString());
-                        }
-                    }
-                }
-            }
-
-            markerData.put("id", state.getId().toString());
-            markerData.put("name", name);
-
-            return "<!--BrightspotCmsFieldAccess " + ObjectUtils.toJson(markerData) + "-->";
+            return FieldAccessFilter.createMarkerHtml(state, name);
         }
 
         /**
-         * Returns IDs of of all objects whose field accesses should be
-         * displayed.
-         *
-         * @param request Can't be {@code null}.
-         * @return Never {@code null}.
+         * @deprecated
+         * @see FieldAccessFilter#getDisplayIds(HttpServletRequest)
          */
+        @Deprecated
         public static Set<UUID> getDisplayIds(HttpServletRequest request) {
-            @SuppressWarnings("unchecked")
-            Set<UUID> displayIds = (Set<UUID>) request.getAttribute(DISPLAY_IDS_ATTRIBUTE);
-
-            if (displayIds == null) {
-                displayIds = new HashSet<UUID>();
-
-                request.setAttribute(DISPLAY_IDS_ATTRIBUTE, displayIds);
-            }
-
-            return displayIds;
+            return FieldAccessFilter.getDisplayIds(request);
         }
     }
 
