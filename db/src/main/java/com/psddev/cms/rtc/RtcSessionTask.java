e68dd30cc9170e263ec7e29bf924e87cdc014f65
Hyoo Lim <hyoolim@gmail.com>
2016-10-25 10:02:09 -0400

    Suppresses all errors during RtcSessionTask execution.

--- db/src/main/java/com/psddev/cms/rtc/RtcSessionTask.java
+++ db/src/main/java/com/psddev/cms/rtc/RtcSessionTask.java
@@ -5,10 +5,14 @@ import com.psddev.dari.db.DatabaseException;
 import com.psddev.dari.db.Query;
 import com.psddev.dari.util.RepeatingTask;
 import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class RtcSessionTask extends RepeatingTask {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RtcSessionTask.class);
 
     @Override
     protected DateTime calculateRunTime(DateTime currentTime) {
@@ -17,27 +21,34 @@ public class RtcSessionTask extends RepeatingTask {
 
     @Override
     protected void doRepeatingTask(DateTime currentTime) throws Exception {
-        long now = Database.Static.getDefault().now();
-        long past = now - 30000;
-        RtcSessionTaskStatus status = Query.from(RtcSessionTaskStatus.class).first();
+        try {
+            long now = Database.Static.getDefault().now();
+            long past = now - 30000;
+            RtcSessionTaskStatus status = Query.from(RtcSessionTaskStatus.class).first();
 
-        if (status.getLastRun() < past) {
-            status.getState().replaceAtomically("lastRun", now);
+            if (status.getLastRun() < past) {
+                status.getState().replaceAtomically("lastRun", now);
 
-            try {
-                status.save();
+                try {
+                    status.save();
 
-            } catch (DatabaseException error) {
-                return;
+                } catch (DatabaseException error) {
+                    return;
+                }
             }
-        }
 
-        Query<RtcSession> query = Query
-                .from(RtcSession.class)
-                .where("lastPing < ?", past);
+            Query<RtcSession> query = Query
+                    .from(RtcSession.class)
+                    .where("lastPing < ?", past);
 
-        for (List<RtcSession> sessions; !(sessions = query.select(0, 100).getItems()).isEmpty();) {
-            sessions.forEach(RtcSession::disconnect);
+            for (List<RtcSession> sessions; !(sessions = query.select(0, 100).getItems()).isEmpty(); ) {
+                sessions.forEach(RtcSession::disconnect);
+            }
+
+        } catch (Throwable error) {
+            LOGGER.debug(
+                    String.format("Can't execute [%s]!", this),
+                    error);
         }
     }
 }
