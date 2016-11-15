de77c78d6eab69f6c7e7c202248eff8f93b8c9a2
Rohit S Monga <rmonga@perfectsensedigital.com>
2016-11-15 17:38:11 -0500

    Adds comments to CORS header addition.

--- db/src/main/java/com/psddev/cms/rtc/RtcFilter.java
+++ db/src/main/java/com/psddev/cms/rtc/RtcFilter.java
@@ -118,6 +118,7 @@ public class RtcFilter extends AbstractFilter implements AbstractFilter.Auto {
                 return;
             }
 
+            // Add CORS header if cross domain and origin matches a site url.
             if (cms.isEnableCrossDomainInlineEditing()) {
                 String origin = request.getHeader("origin");
 
