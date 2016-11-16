5bb83e2840133d4015acc213abc4bd72cecca7c2
Kevin Young <kyoung@perfectsensedigital.com>
2016-11-16 12:33:24 -0500

    removes unused imports

--- db/src/main/java/com/psddev/cms/db/ToolUserDictionary.java
+++ db/src/main/java/com/psddev/cms/db/ToolUserDictionary.java
@@ -2,9 +2,7 @@ package com.psddev.cms.db;
 
 import com.psddev.dari.db.Record;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Locale;
 import java.util.Set;
 import java.util.UUID;
