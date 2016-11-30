a25a6521248f340e740e16a29785923d172726f2
Hyoo Lim <hyoolim@gmail.com>
2016-11-30 15:38:30 -0500

    Fixes draft in workflow publishing not to change the publish info.

--- db/src/main/java/com/psddev/cms/tool/ToolPageContext.java
+++ db/src/main/java/com/psddev/cms/tool/ToolPageContext.java
@@ -3490,6 +3490,7 @@ public class ToolPageContext extends WebPageContext {
         State state = State.getInstance(object);
         boolean newContent = state.isNew() || !state.isVisible();
         Content.ObjectModification contentData = state.as(Content.ObjectModification.class);
+        Draft draft = getOverlaidDraft(object);
         ToolUser user = getUser();
 
         if (state.isNew()
@@ -3499,13 +3500,12 @@ public class ToolPageContext extends WebPageContext {
             if (getContentFormPublishDate() != null) {
                 setContentFormScheduleDate(object);
 
-            } else {
+            } else if (draft == null) {
                 contentData.setPublishDate(new Date());
                 contentData.setPublishUser(user);
             }
         }
 
-        Draft draft = getOverlaidDraft(object);
         UUID variationId = param(UUID.class, "variationId");
         Site site = getSite();
 
